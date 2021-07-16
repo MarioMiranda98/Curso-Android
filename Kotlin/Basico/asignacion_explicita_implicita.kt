d5574efe2959a401d952af95b57dab8b8dcbc134
MarioMiranda98 <FAROLIN95@msn.com>
2021-07-15 19:04:12 -0500

    first commit

---
+++ Kotlin/Basico/asignacion_explicita_implicita.kt
@@ -0,0 +1,17 @@
+/*
+ Tipos de datos
+ Tipos numericos Bit Width
+ Double				64
+ Float				32
+ Long				64
+ Int				32
+ Short 				16
+ Byte				8
+ 
+ Characters, Strings, Arrays, Booleans 
+ */
+
+fun main() {
+	var numero = 18 //Asignacion implicita
+    var numero2:Int = 20 //Asignacion Explicita
+}
---
+++ Kotlin/Basico/variables_constantes.kt
@@ -0,0 +1,16 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+   //Variables
+   var edadCabezita = 5
+   println("Edad de Cabezita: ${edadCabezita}")
+   edadCabezita = 6
+   println("Edad de Cabezita un año despues ${edadCabezita}")
+    
+   //Constantes
+   val nombreCabezita = "Cabezita"
+   println("Nombre del perro: ${nombreCabezita}")
+}
